6fcd5bad7c469fe4d95481bd9e1170ab9e3097f8
scott <scott@derossi.consulting>
2025-11-17 23:27:45 +0000

    Full functionality

--- BookooScale.py
+++ BookooScale.py
@@ -3,6 +3,13 @@ import asyncio
 from bleak import BleakScanner, BleakClient
 
 WEIGHT_UUID = "0000ff11-0000-1000-8000-00805f9b34fb"
+COMMAND_UUID = "0000ff12-0000-1000-8000-00805f9b34fb"
+
+TARE_PACKET = bytearray([0x03, 0x0A, 0x01, 0x00, 0x00, 0x08])
+TIMER_START_PACKET = bytearray([0x03, 0x0A, 0x04, 0x00, 0x00, 0x0A])
+TIMER_STOP_PACKET = bytearray([0x03, 0x0A, 0x05, 0x00, 0x00, 0x0D])
+TIMER_RESET_PACKET = bytearray([0x03, 0x0A, 0x06, 0x00, 0x00, 0x0C])
+TARE_AND_TIMER_START_PACKET = bytearray([0x03, 0x0A, 0x07, 0x00, 0x00, 0x00])
 
 
 class BookooScale:
@@ -17,6 +24,9 @@ class BookooScale:
 
         if self.address is None:
             await self.discover_device()
+        if self.address is None:
+            print("Failed to establish connection.")
+            return False
 
         self.client = BleakClient(self.address)
         success = await self.connect()
@@ -44,16 +54,23 @@ class BookooScale:
         print("Connecting...")
         await self.client.connect()
         asyncio.create_task(self.poll_weight())
+        while self.weight is None:
+            await asyncio.sleep(0.05)
         return self.connected()
 
     def connected(self):
+        if not self.client:
+            return False
         return self.client.is_connected
 
     async def disconnect(self):
-        print("Disconnecting...")
-        await self.client.disconnect()
-        self.weight = None
-        return not self.connected()
+        if self.connected():
+            print("Disconnecting...")
+            await self.client.disconnect()
+            self.weight = None
+            return not self.connected()
+        else:
+            print("No device connected to disconnect from.")
 
     def on_weight(self, sender, data: bytearray):
         if len(data) != 20:
@@ -71,6 +88,34 @@ class BookooScale:
 
     async def poll_weight(self):
         await self.client.start_notify(WEIGHT_UUID, self.on_weight)
+
+    async def _send_command(self, data1, data2, data3, datasum=0x00):
+        """Send a 6-byte command to the scale (checksum not required)."""
+        if self.client is None or not self.client.is_connected:
+            raise Exception("Not connected")
+
+        packet = bytearray([0x03, 0x0A, data1, data2, data3, datasum])
+        await self.client.write_gatt_char(COMMAND_UUID, packet)
+
+    async def send_tare(self):
+        print("Tare.")
+        print(await self.client.write_gatt_char(COMMAND_UUID, TARE_PACKET))
+
+    async def send_timer_start(self):
+        print("Start Timer.")
+        await self.client.write_gatt_char(COMMAND_UUID, TIMER_START_PACKET)
+
+    async def send_timer_stop(self):
+        print("Stop Timer.")
+        await self.client.write_gatt_char(COMMAND_UUID, TIMER_STOP_PACKET)
+
+    async def send_timer_reset(self):
+        print("Reset Timer.")
+        await self.client.write_gatt_char(COMMAND_UUID, TIMER_RESET_PACKET)
+
+    async def send_tare_and_timer_start(self):
+        print("Tare and Start Timer.")
+        await self.client.write_gatt_char(COMMAND_UUID, TARE_AND_TIMER_START_PACKET)
 
     def _is_bookoo_device(self, device):
         if device.name is None:
--- main.py
+++ main.py
@@ -9,12 +9,20 @@ async def main():
     assert await scale.establish_connection()
 
     try:
-        while True:
-            print(scale.weight)
-            await asyncio.sleep(0.1)
+        await scale.send_tare_and_timer_start()
+        print(scale.weight)
+        await asyncio.sleep(3.5)
+        print(scale.weight)
+        await scale.send_timer_stop()
+        print(scale.weight)
+        await asyncio.sleep(3)
+        print(scale.weight)
+        await scale.send_timer_reset()
+        print(scale.weight)
     except Exception as e:
         # Disconnect gracefully in case of any issues
         print("Exception: " + str(e))
         await scale.disconnect()
+    await scale.disconnect()
 
 asyncio.run(main())
