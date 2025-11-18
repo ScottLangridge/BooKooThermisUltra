0d9c04bce132787aca83b29b19c07ebb277c42fd
scott <scott@derossi.consulting>
2025-11-18 12:31:43 +0000

    Full functionality POC

--- scale_poc.py
+++ scale_poc.py
@@ -9,16 +9,21 @@ async def main():
     assert await scale.establish_connection()
 
     try:
+        # Tare and count to three.
         await scale.send_tare_and_timer_start()
-        print(scale.weight)
-        await asyncio.sleep(3.5)
-        print(scale.weight)
+        await asyncio.sleep(3.1)
         await scale.send_timer_stop()
-        print(scale.weight)
+
+        # Wait three seconds and reset
         await asyncio.sleep(3)
-        print(scale.weight)
         await scale.send_timer_reset()
-        print(scale.weight)
+
+        # Loop weight
+        while True:
+            print(str(scale.weight) + "g")
+            await asyncio.sleep(0.1)
+
+
     except Exception as e:
         # Disconnect gracefully in case of any issues
         print("Exception: " + str(e))
